52eec01dfe53532763f0d27f2cc07fbaf4fb276c
xurunhua.nwpu@gmail.com <xurunhua.nwpu@gmail.com@f417c0db-510d-a7ef-85bc-c2e97766275d>
2010-05-04 15:52:19 +0000

    添加了PictureDaoManager(未完成) 修改了applicationContext中关于所有*DaoManager类的事务声明管理方式，暂且缩小到UserDaoManager 修改了AlbumDaoManager中的事务管理改为Sping编程式事物管理，检测可否为Flex远程调用

--- src/com/goodfriend/daomanager/impl/AlbumDaoManager.java
+++ src/com/goodfriend/daomanager/impl/AlbumDaoManager.java
@@ -2,6 +2,9 @@ package com.goodfriend.daomanager.impl;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.goodfriend.dao.IAlbumDAO;
 import com.goodfriend.daomanager.IAlbumDaoManager;
 import com.goodfriend.model.Album;
@@ -10,24 +13,29 @@ public class AlbumDaoManager implements IAlbumDaoManager {
 	
 	private IAlbumDAO albumDao;
 	
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = false)
 	public void addAlbum(Album album) {
 		albumDao.save(album);
 	}
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = false)
 	public void deleteAlbum(Album album) {
 		albumDao.delete(album);
 	}
 
+	@Transactional(readOnly = false)
 	public List<Album> getAlbumByProperty(String propertyName, Object value) {
 		List<Album> albums = albumDao.findByProperty(propertyName, value);
 		return albums;
 	}
 
+	@Transactional(readOnly = false)
 	public List<Album> getAllAlbums() {
 		List<Album> albums = albumDao.findAll();
 		return albums;
 	}
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = false)
 	public void updateAlbum(Album album) {
 		albumDao.attachDirty(album);
 	}
